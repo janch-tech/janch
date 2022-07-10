e178c0b8ec86f7c0004579e330c9158898c1f60f
taarimalta <taarimalta@gmail.com>
2022-07-10 15:33:09 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A YAML config centric tool to act on your service status",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/janch-tech/janch",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
